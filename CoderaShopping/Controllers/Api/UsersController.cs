bcc0b260294b71d28e7cdf85bb90e2df3c29be7e
Hristijan Bozhijanov <hristijan.bozhijanov@intellaquest.com>
2023-04-25 10:14:49 +0200

    clean up

--- CoderaShopping/Controllers/Api/UsersController.cs
+++ CoderaShopping/Controllers/Api/UsersController.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Web;
-using System.Web.Mvc;
-using CoderaShopping.App_Start;
-using CoderaShopping.Business.Models;
-using CoderaShopping.Business.Services;
+﻿using System.Web.Mvc;
 
 namespace CoderaShopping.Controllers.Api
 {
